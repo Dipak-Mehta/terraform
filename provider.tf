5904c0d242ec9f2cc5347681a1f24bad069dde7c
Dipak mehta <101636827+Dipak-Mehta@users.noreply.github.com>
2022-11-11 12:01:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAXR5RIFFVLM5SDX6X"
-  secret_key = "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"
+  access_key = "AKIAXR5RyIFFVLM5SDX6X"
+  secret_key = "B38EfhvFcK5NwMWwGNBME4YDspwsxUhEUW5kfAdw3"
 }
