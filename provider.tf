3789c41943807651731bd701183e017908d7cabc
Dipak mehta <101636827+Dipak-Mehta@users.noreply.github.com>
2025-04-19 18:09:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAXR5RyIFFVLM5SDX6X"
-  secret_key = "B38EfhvFcK5NwMWwGNBME4YDspwsxUhEUW5kfAdw3"
+  access_key = ""
+  secret_key = ""
 }
