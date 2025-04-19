af8cea071ce8f5f43efee550f0ffc0510deaa054
Dipak mehta <101636827+Dipak-Mehta@users.noreply.github.com>
2025-04-19 18:12:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "mysg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["152.57.104.119/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
