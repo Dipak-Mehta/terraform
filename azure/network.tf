2c73b39ee5ada27482f1c11d37942e2610d4a5ee
Dipak mehta <101636827+Dipak-Mehta@users.noreply.github.com>
2026-09-25 10:57:23 +0530

    feat: add reusable Azure and GCP Terraform infrastructure

---
+++ azure/network.tf
@@ -0,0 +1,75 @@
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = "${var.project_name}-vnet"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = [var.vnet_cidr]
+
+  tags = {
+    Project = var.project_name
+  }
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = "${var.project_name}-public-subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [var.public_subnet_cidr]
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = "${var.project_name}-private-subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [var.private_subnet_cidr]
+}
+
+resource "azurerm_public_ip" "public_vm" {
+  name                = "${var.project_name}-public-ip"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  tags = {
+    Project = var.project_name
+  }
+}
+
+resource "azurerm_nat_gateway" "this" {
+  name                    = "${var.project_name}-nat"
+  location                = azurerm_resource_group.this.location
+  resource_group_name     = azurerm_resource_group.this.name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+
+  tags = {
+    Project = var.project_name
+  }
+}
+
+resource "azurerm_public_ip" "nat" {
+  name                = "${var.project_name}-nat-ip"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  tags = {
+    Project = var.project_name
+  }
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "this" {
+  nat_gateway_id       = azurerm_nat_gateway.this.id
+  public_ip_address_id = azurerm_public_ip.nat.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "private" {
+  subnet_id      = azurerm_subnet.private.id
+  nat_gateway_id = azurerm_nat_gateway.this.id
+}
