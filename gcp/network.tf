9a9816a088924d500596784b7142ccf785a92538
Dipak mehta <101636827+Dipak-Mehta@users.noreply.github.com>
2026-09-25 10:57:51 +0530

    feat: add reusable Azure and GCP Terraform infrastructure

---
+++ gcp/network.tf
@@ -0,0 +1,41 @@
+resource "google_compute_network" "this" {
+  name                    = "${var.project_name}-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "public" {
+  name          = "${var.project_name}-public-subnet"
+  ip_cidr_range = var.public_subnet_cidr
+  region        = var.region
+  network       = google_compute_network.this.id
+}
+
+resource "google_compute_subnetwork" "private" {
+  name          = "${var.project_name}-private-subnet"
+  ip_cidr_range = var.private_subnet_cidr
+  region        = var.region
+  network       = google_compute_network.this.id
+}
+
+resource "google_compute_router" "this" {
+  name    = "${var.project_name}-router"
+  region  = var.region
+  network = google_compute_network.this.id
+}
+
+resource "google_compute_router_nat" "this" {
+  name                               = "${var.project_name}-nat"
+  router                             = google_compute_router.this.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.private.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
+
+resource "google_compute_global_address" "reserved" {
+  name = "${var.project_name}-reserved-ip"
+}
